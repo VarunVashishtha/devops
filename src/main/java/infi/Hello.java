3ecb8ebb8c13e12d32760e84d8c8402e32cc3002
Varun Vashishtha <varun.vashishtha@nagarro.com>
2019-03-22 22:15:00 +0530

    first commit

---
+++ src/main/java/infi/Hello.java
@@ -0,0 +1,7 @@
+package infi;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/infi/HelloTest.java
@@ -0,0 +1,13 @@
+package infi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloTest {
+
+    @Test
+    public void main() {
+        System.out.println("Success");
+    }
+}
